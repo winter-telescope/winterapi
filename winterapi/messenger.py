320d26e13affa9bb67ccf7fc96844a87346edd62
Robert Stein <rdstein@caltech.edu>
2024-01-31 18:06:11 -0800

    Update docs
    Get schedules working
    
    Update example
    
    Update to allow ToO deletes
    
    Check minimal server version
    
    Add image download
    
    Cleanup

--- winterapi/messenger.py
+++ winterapi/messenger.py
@@ -1,16 +1,27 @@
 """
 This module contains the messenger, which is used to communicate with the API
 """
+
 import getpass
-import json
 import logging
+from importlib import metadata
+from pathlib import Path
 from typing import Optional
 
-import backoff
 import pandas as pd
 import requests
-from pydantic import BaseModel
-from wintertoo.models import Program
+from astropy import units as u
+from astropy.time import Time
+from packaging import version
+from wintertoo.models import (
+    DEFAULT_IMAGE_TYPE,
+    ConeImageQuery,
+    ImagePath,
+    Program,
+    ProgramImageQuery,
+    RectangleImageQuery,
+    winter_image_types,
+)
 from wintertoo.models.too import (
     AllTooClasses,
     Summer,
@@ -21,30 +32,76 @@ from wintertoo.models.too import (
     WinterRaDecToO,
 )
 from wintertoo.schedule import concat_toos
+from wintertoo.utils import get_date
 
+from winterapi.base_api import MAX_TIMEOUT, BaseAPI
 from winterapi.endpoints import (
+    DOWNLOAD_LIST_URL,
+    IMAGE_QUERY_URL,
     PING_URL,
     PROGRAM_URL,
+    SCHEDULE_DELETE_URL,
+    SCHEDULE_DETAILS_URL,
+    SCHEDULE_SUMMARY_URL,
     SUMMER_TOO_URL,
     USER_URL,
+    VERSION_URL,
     WINTER_TOO_URL,
 )
 from winterapi.fidelius import Fidelius
 
 logger = logging.getLogger(__name__)
 
-MAX_TIMEOUT = 30
 
-
-class WinterAPI:
+class WinterAPI(BaseAPI):
     """
     Class to communicate with the Winter API
     """
 
     def __init__(self):
         self.fidelius = Fidelius()
-        logger.info(f"API ping success is {self.ping()}")
+        ping = self.ping()
+        if not ping:
+            logger.warning("Could not successfully ping server")
+        logger.info(f"API ping success is {ping}")
         self.auth = (None, None)
+        self.check_version()
+
+    @staticmethod
+    def ping():
+        """
+        Ping the API.
+
+        :return: boolean for success
+        """
+        try:
+            res = requests.get(PING_URL, timeout=MAX_TIMEOUT)
+            res.raise_for_status()
+            return res.status_code == 200
+        except requests.exceptions.ConnectionError:
+            return False
+
+    @staticmethod
+    def check_version():
+        """
+        Check the version of the API.
+
+        :return: API response
+        """
+        res = requests.get(VERSION_URL, timeout=MAX_TIMEOUT)
+        if res.status_code == 200:
+            server_version = version.parse(res.json()["body"])
+            local_version = version.parse(metadata.version("winterapi"))
+            logger.info(f"Server requires minimum winterapi version: {server_version}")
+            logger.info(f"Local winterapi version: {local_version}")
+            if server_version > local_version:
+                logger.warning(
+                    f"Local winterapi version ({local_version}) is out of date! "
+                    f"Server requires a minimum of {server_version}. "
+                    f"Please update winterapi."
+                )
+        else:
+            logger.warning("Could not check minimum version of winterapi for server")
 
     @staticmethod
     def clear_cache():
@@ -64,79 +121,6 @@ class WinterAPI:
         if self.auth == (None, None):
             self.auth = (self.fidelius.get_user(), self.fidelius.get_password())
         return self.auth
-
-    @backoff.on_exception(
-        backoff.expo, requests.exceptions.RequestException, max_time=MAX_TIMEOUT
-    )
-    def get(self, url, auth=None, **kwargs) -> requests.Response:
-        """
-        Run a get request.
-
-        :param url: URL to get.
-        :param auth: Authentication details.
-        :param kwargs: additional arguments for API.
-        :return: API response.
-        """
-        if auth is None:
-            auth = self.get_auth()
-        res = requests.get(url, auth=auth, params=kwargs, timeout=MAX_TIMEOUT)
-
-        if res.status_code != 200:
-            err = f"API call failed with '{res}: {res.text}'"
-            logger.error(err)
-            raise ValueError(err)
-        return res
-
-    @backoff.on_exception(
-        backoff.expo, requests.exceptions.RequestException, max_time=MAX_TIMEOUT
-    )
-    def post(
-        self, url, data: AllTooClasses | list[AllTooClasses], auth=None, **kwargs
-    ) -> requests.Response:
-        """
-        Run a post request.
-
-        :param url: URL to post to.
-        :param data: Data to post.
-        :param auth: Authentication details.
-        :param kwargs: additional arguments for API.
-        :return: Response.
-        """
-        if auth is None:
-            auth = self.get_auth()
-
-        if isinstance(data, list):
-            convert = json.dumps([x.model_dump() for x in data])
-        elif isinstance(data, BaseModel):
-            convert = json.dumps([data.model_dump()])
-        else:
-            err = f"Unrecognised data type {type(data)}"
-            logger.error(err)
-            raise TypeError(err)
-
-        res = requests.post(
-            url, data=convert, auth=auth, params=kwargs, timeout=MAX_TIMEOUT
-        )
-
-        if res.status_code != 200:
-            err = f"API call failed with '{res}: {res.text}'"
-            logger.error(err)
-            raise ValueError(err)
-        return res
-
-    @staticmethod
-    def ping():
-        """
-        Ping the API.
-
-        :return: boolean for success
-        """
-        try:
-            res = requests.get(PING_URL, timeout=MAX_TIMEOUT)
-            res.raise_for_status()
-            return res.status_code == 200
-        except ConnectionError:
-            return False
 
     def add_user_details(
         self,
@@ -290,7 +274,7 @@ class WinterAPI:
         if not isinstance(data, list):
             data = [data]
         for entry in data:
-            assert isinstance(entry, Winter)
+            assert isinstance(entry, Winter), f"Entry {entry} is not a Winter ToO"
         return self._submit_too(
             program_name=program_name,
             url=WINTER_TOO_URL,
@@ -335,3 +319,279 @@ class WinterAPI:
         """
         program = self.get_program_details(program_name=program_name)
         return concat_toos(data, program=program)
+
+    def get_observatory_queue(
+        self,
+        program_name: str,
+    ) -> tuple[requests.Response, pd.DataFrame]:
+        """
+        Function to get the observatory queue
+
+        :param program_name: Name of the program under which to check ToOs
+        :return: API response and TOO schedule
+        """
+
+        program = self.get_program_details(program_name=program_name)
+
+        res = self.get(
+            SCHEDULE_SUMMARY_URL,
+            program_name=program_name,
+            program_api_key=program.prog_key,
+        )
+
+        observatory_queue = pd.DataFrame(res.json()["body"])
+        return res, observatory_queue
+
+    def get_too_details(
+        self,
+        program_name: str,
+        too_schedule_name: str,
+    ) -> tuple[requests.Response, pd.DataFrame]:
+        """
+        Function to get the details of a single queued TOO schedule
+
+        :param program_name: Name of the program under which TOO was submitted
+        :param too_schedule_name: Name of the TOO schedule
+        :return: API response and TOO schedule
+        """
+
+        program = self.get_program_details(program_name=program_name)
+
+        res = self.get(
+            SCHEDULE_DETAILS_URL,
+            program_name=program_name,
+            program_api_key=program.prog_key,
+            schedule_name=too_schedule_name,
+        )
+
+        too_schedule = pd.DataFrame(res.json()["body"])
+        return res, too_schedule
+
+    def delete_too_request(
+        self,
+        program_name: str,
+        too_schedule_name: str,
+    ) -> requests.Response:
+        """
+        Function to delete a queued TOO schedule
+
+        :param program_name: Name of the program under which TOO was submitted
+        :param too_schedule_name: Name of the TOO schedule
+        :return: API response
+        """
+
+        program = self.get_program_details(program_name=program_name)
+
+        res = self.delete(
+            SCHEDULE_DELETE_URL,
+            program_name=program_name,
+            program_api_key=program.prog_key,
+            schedule_name=too_schedule_name,
+        )
+
+        return res
+
+    def query_images(
+        self,
+        query: ProgramImageQuery | RectangleImageQuery | ConeImageQuery,
+    ) -> tuple[requests.Response, pd.DataFrame]:
+        """
+        Function to get the observatory queue
+
+        :param query: Query Request
+        :return: API response and TOO schedule
+        """
+
+        program = self.get_program_details(program_name=query.program_name)
+
+        res = self.get(
+            IMAGE_QUERY_URL,
+            program_name=query.program_name,
+            program_api_key=program.prog_key,
+            data=[query],
+        )
+
+        image_summary = pd.DataFrame(res.json()["body"])
+        return res, image_summary
+
+    @staticmethod
+    def check_query_dates(
+        start_date: str | None = None,
+        end_date: str | None = None,
+    ) -> tuple[str, str]:
+        """
+        Function to check the dates
+
+        :param start_date:
+        :param end_date:
+        :return:
+        """
+        if start_date is None:
+            start_date = get_date(Time.now() - 30.0 * u.day)
+
+        if end_date is None:
+            end_date = get_date(Time.now())
+
+        return start_date, end_date
+
+    def query_images_by_program(
+        self,
+        program_name: str,
+        start_date: str | None = None,
+        end_date: str | None = None,
+        image_type: winter_image_types = "science",
+    ) -> tuple[requests.Response, pd.DataFrame]:
+        """
+        Function to get the observatory queue
+
+        :param program_name: Name of the program under which to check ToOs
+        :param start_date: Start date for images
+        :param end_date: End date for images
+        :param image_type: Type of image to query
+        :return: API response and TOO schedule
+        """
+
+        start_date, end_date = self.check_query_dates(
+            start_date=start_date, end_date=end_date
+        )
+
+        print(
+            f"Querying images for {program_name} between "
+            f"{start_date} and {end_date} of type '{image_type}'"
+        )
+
+        query = ProgramImageQuery(
+            program_name=program_name,
+            start_date=start_date,
+            end_date=end_date,
+            kind=image_type,
+        )
+
+        return self.query_images(query=query)
+
+    def query_images_by_cone(
+        self,
+        program_name: str,
+        ra_deg: float,
+        dec_deg: float,
+        radius_deg: float = 1.0,
+        start_date: str | None = None,
+        end_date: str | None = None,
+        image_type: winter_image_types = DEFAULT_IMAGE_TYPE,
+    ) -> tuple[requests.Response, pd.DataFrame]:
+        """
+        Function to get the observatory queue
+
+        :param program_name: Name of the program under which to check ToOs
+        :param ra_deg: Right Ascension in degrees
+        :param dec_deg: Declination in degrees
+        :param radius_deg: Radius in degrees
+        :param start_date: Start date for images
+        :param end_date: End date for images
+        :param image_type: Type of image to query
+        :return: API response and TOO schedule
+        """
+
+        start_date, end_date = self.check_query_dates(
+            start_date=start_date, end_date=end_date
+        )
+
+        print(
+            f"Querying images for {program_name} between "
+            f"{start_date} and {end_date} of type '{image_type}', "
+            f"with a radius of {radius_deg} degrees around {ra_deg}, {dec_deg}"
+        )
+
+        query = ConeImageQuery(
+            program_name=program_name,
+            ra=ra_deg,
+            dec=dec_deg,
+            radius_deg=radius_deg,
+            start_date=start_date,
+            end_date=end_date,
+            kind=image_type,
+        )
+
+        return self.query_images(query=query)
+
+    def query_images_by_rectangle(
+        self,
+        program_name: str,
+        ra_min_deg: float,
+        ra_max_deg: float,
+        dec_min_deg: float,
+        dec_max_deg: float,
+        start_date: str | None = None,
+        end_date: str | None = None,
+        image_type: winter_image_types = DEFAULT_IMAGE_TYPE,
+    ) -> tuple[requests.Response, pd.DataFrame]:
+        """
+        Function to get the observatory queue
+
+        :param program_name: Name of the program under which to check ToOs
+        :param ra_min_deg: Minimum Right Ascension in degrees
+        :param ra_max_deg: Maximum Right Ascension in degrees
+        :param dec_min_deg: Minimum Declination in degrees
+        :param dec_max_deg: Maximum Declination in degrees
+        :param start_date: Start date for images
+        :param end_date: End date for images
+        :param image_type: Type of image to query
+        :return: API response and TOO schedule
+        """
+
+        start_date, end_date = self.check_query_dates(
+            start_date=start_date, end_date=end_date
+        )
+
+        print(
+            f"Querying images for {program_name} between "
+            f"{start_date} and {end_date} of type '{image_type}', "
+            f"with RA between {ra_min_deg} and {ra_max_deg} and "
+            f"Dec between {dec_min_deg} and {dec_max_deg}"
+        )
+
+        query = RectangleImageQuery(
+            program_name=program_name,
+            ra_min=ra_min_deg,
+            ra_max=ra_max_deg,
+            dec_min=dec_min_deg,
+            dec_max=dec_max_deg,
+            start_date=start_date,
+            end_date=end_date,
+            kind=image_type,
+        )
+
+        return self.query_images(query=query)
+
+    def download_image_list(
+        self,
+        program_name: str,
+        paths: list[str] | str,
+        image_type: winter_image_types,
+        output_dir: str | None | Path = None,
+    ) -> tuple[requests.Response, Path]:
+        """
+        Download images as a zip file.
+
+        :param program_name: Name of the program under which to check ToOs
+        :param image_type: Type of image to query
+        :param output_dir: Directory to save the zip to
+        :param paths: List of paths to download
+        :return: API response
+        """
+
+        if not isinstance(paths, list):
+            paths = [paths]
+
+        program = self.get_program_details(program_name=program_name)
+
+        res, output_path = self.get_stream(
+            DOWNLOAD_LIST_URL,
+            savepath=output_dir,
+            program_name=program_name,
+            program_api_key=program.prog_key,
+            data=[ImagePath(path=x) for x in paths],
+            kind=image_type,
+        )
+
+        return res, output_path
